0dca2dad44bbed268c95b2dcbe12c814b5bf3e0c
seonghoon <shryu@e-hcg.com>
2024-10-22 21:32:18 +0900

    Update blog contents

--- craco.config.js
+++ craco.config.js
@@ -1,12 +1,34 @@
+const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
   webpack: {
     configure: (webpackConfig) => {
+      // 마크다운 파일 로더 설정
       webpackConfig.module.rules.push({
         test: /\.md$/,
         type: 'asset/source'
       });
+
+      // Node.js 폴리필 설정
+      webpackConfig.resolve = {
+        ...webpackConfig.resolve,
+        fallback: {
+          ...webpackConfig.resolve?.fallback,
+          "buffer": require.resolve("buffer/"),
+          "process": require.resolve("process/browser"),
+        }
+      };
+
+      // 플러그인 추가
+      webpackConfig.plugins = [
+        ...webpackConfig.plugins,
+        new webpack.ProvidePlugin({
+          Buffer: ['buffer', 'Buffer'],
+          process: 'process/browser'
+        })
+      ];
+
       return webpackConfig;
     }
   }
